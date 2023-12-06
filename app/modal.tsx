e4d6bf192e210a0702ea97ce23368cad3bb630d9
ElianRenteria <elianrenteriadevelopment@gmail.com>
2023-12-06 02:40:11 -0800

    added style

--- app/modal.tsx
+++ app/modal.tsx
@@ -3,6 +3,8 @@ import { Platform, StyleSheet } from 'react-native';
 import * as React from 'react';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import styles from '../styles/lotOverviewModal';
+
 
 export default function ModalScreen() {
   return (
@@ -17,19 +19,4 @@ export default function ModalScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+
